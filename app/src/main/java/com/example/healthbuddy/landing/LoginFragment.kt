6ee90dddc105c0dc86ee2bf2e6960e51a9e1f2d4
jialiiiiii <chiajiali02@gmail.com>
2023-09-17 00:10:48 +0800

    setting up firebase

--- app/src/main/java/com/example/healthbuddy/landing/LoginFragment.kt
+++ app/src/main/java/com/example/healthbuddy/landing/LoginFragment.kt
@@ -1,24 +1,98 @@
 package com.example.healthbuddy.landing
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.TextAppearanceSpan
+import android.text.style.UnderlineSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.healthbuddy.R
+import com.example.healthbuddy.databinding.LandingLoginFragmentBinding
 
 class LoginFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
+    private lateinit var binding: LandingLoginFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.landing_login_fragment, container, false)
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.landing_login_fragment,
+            container,
+            false
+        )
+
+        // Find the TextView that contains disclaimer
+        val disclaimerTextView = binding.disclaimer
+
+        // Retrieve text from strings.xml
+        val termsAndConditionsText = getString(R.string.terms_and_conditions)
+        val privacyPolicyText = getString(R.string.privacy_policy)
+        val disclaimerTemplate = getString(R.string.disclaimer)
+
+        // Replace placeholders with underlined and clickable text
+        val spannableDisclaimer = SpannableString(disclaimerTemplate)
+
+        // Find the positions of "Terms and Conditions" and "Privacy Policy" within the disclaimer text
+        val termsAndConditionsStart = spannableDisclaimer.indexOf(termsAndConditionsText)
+        val privacyPolicyStart = spannableDisclaimer.indexOf(privacyPolicyText)
+
+        // Apply underline and click listener to "Terms and Conditions"
+        spannableDisclaimer.setSpan(
+            UnderlineSpan(),
+            termsAndConditionsStart,
+            termsAndConditionsStart + termsAndConditionsText.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableDisclaimer.setSpan(
+            object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    // Navigate to the Terms and Conditions fragment
+                    findNavController().navigate(R.id.action_login_to_terms)
+                }
+            },
+            termsAndConditionsStart,
+            termsAndConditionsStart + termsAndConditionsText.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        // Apply underline and click listener to "Privacy Policy"
+        spannableDisclaimer.setSpan(
+            UnderlineSpan(),
+            privacyPolicyStart,
+            privacyPolicyStart + privacyPolicyText.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableDisclaimer.setSpan(
+            object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    // Navigate to the Privacy Policy fragment
+                    findNavController().navigate(R.id.action_login_to_privacy)
+                }
+            },
+            privacyPolicyStart,
+            privacyPolicyStart + privacyPolicyText.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        // Set the formatted text in the TextView and enable clickable links
+        disclaimerTextView.text = spannableDisclaimer
+        disclaimerTextView.movementMethod = LinkMovementMethod.getInstance()
+
+        // Set the view's root from the binding object
+        return binding.root
     }
 
 }
---
+++ app/src/main/java/com/example/healthbuddy/settings/PrivacyPolicyFragment.kt
@@ -0,0 +1,80 @@
+package com.example.healthbuddy.settings
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.databinding.DataBindingUtil
+import com.example.healthbuddy.R
+import com.example.healthbuddy.databinding.PrivacyPolicyFragmentBinding
+import java.io.IOException
+import java.io.InputStream
+
+class PrivacyPolicyFragment : Fragment() {
+
+    private lateinit var binding: PrivacyPolicyFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.privacy_policy_fragment,
+            container,
+            false
+        )
+
+        binding.imageView.setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+        // Find the WebView
+        val webView = binding.webView
+        val progressBar = binding.progressBar
+
+        // Determine the selected language
+        val selectedLanguage = "en"
+
+        // Define the file name based on the selected language
+        val fileName = "privacy_policy_$selectedLanguage.html"
+
+        try {
+            // Open and read the HTML file from the assets folder
+            val assetManager = requireContext().assets
+            val inputStream: InputStream = assetManager.open(fileName)
+
+            // Enable JavaScript for a better WebView experience
+            webView.settings.javaScriptEnabled = true
+
+            // Load the HTML content into the WebView
+            webView.loadDataWithBaseURL(null, inputStream.reader().readText(), "text/html", "UTF-8", null)
+
+            // Ensure links open in the WebView, not in an external browser
+            webView.webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    // Show the progress bar when a page starts loading
+                    progressBar.visibility = View.VISIBLE
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    // Hide the progress bar when the page has finished loading
+                    progressBar.visibility = View.GONE
+                }
+            }
+
+        } catch (e: IOException) {
+            // Handle any exceptions that may occur while reading the file
+            e.printStackTrace()
+        }
+
+        return binding.root
+    }
+
+}
