7868617be282f1a3dc97736aa7f8cd2194ef314e
jialiiiiii <chiajiali02@gmail.com>
2023-09-24 18:55:20 +0800

    fix layout bug when lang changed

--- app/src/main/java/com/example/healthbuddy/forum/ForumDetailsFragment.kt
+++ app/src/main/java/com/example/healthbuddy/forum/ForumDetailsFragment.kt
@@ -6,7 +6,6 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Base64
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,7 +17,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.example.healthbuddy.database.Collection
 import com.example.healthbuddy.databinding.FragmentForumDetailsBinding
-import com.example.healthbuddy.post.DtClass
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
